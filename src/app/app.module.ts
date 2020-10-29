349d74ae910d71e2e6b1ecc2c3bc5f8c5c4314cd
joaquin <joaquinlifs0206@gmail.com>
2020-10-29 13:39:46 -0300

    instalacion angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,15 @@ import { TareaComponent } from './tarea/tarea.component';
 //Libreria importada flexLayout https://github.com/angular/flex-layout
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { PeliculaComponent } from './pelicula/pelicula.component';
+
 import { VistaPeliculaComponent } from './vista-pelicula/vista-pelicula.component';
+// modulos para angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +46,7 @@ import { VistaPeliculaComponent } from './vista-pelicula/vista-pelicula.componen
     PeliculaComponent,
     VistaPeliculaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, FlexLayoutModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, FlexLayoutModule, BrowserAnimationsModule, MatSliderModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   providers: [],
   bootstrap: [AppComponent],
 })
